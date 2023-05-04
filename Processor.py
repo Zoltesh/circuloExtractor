c26e6a7613365588d30d0c764e07f95c53a449ef
Zoltesh <65920778+Zoltesh@users.noreply.github.com>
2023-05-03 19:31:31 -0600

    Working program with FICO and General information. Will incorporate the remaining data points next.

--- Processor.py
+++ Processor.py
@@ -4,58 +4,11 @@ Data processing and concurrent operations
 
 import os
 from pathlib import Path
-import pandas as pd
-import pdfplumber
-from concurrent.futures import ThreadPoolExecutor
-
-
-# Extract data from a single pdf and create a pandas dataframe
-def extract_and_process_pdf(input_path: str):
-    # Extract text from the input PDF
-    text = ''
-    with pdfplumber.open(input_path) as pdf:
-        for page in pdf.pages:
-            text += page.extract_text()
-
-    # Process the extracted text and create a pandas DataFrame (data structure)
-    # ...
-    pass
-    """return pd.DataFrame(data)"""
-
-
-# Create prepare data from a single dataframe to create data visualizations and fill out the output pdf
-def process_dataframe(input_path: str, output_path: str):
-    # Process each PDF in the input folder
-    for file in os.listdir(input_path):
-        if file.endswith(".pdf"):
-            input_path = os.path.join(input_path, file)
-
-            # Extract text from the input PDF
-            text = ''
-            with pdfplumber.open(input_path) as pdf:
-                for page in pdf.pages:
-                    text += page.extract_text()
-            print(text)
-
-            # Process the extracted text, create data structures, etc.
-            # ...
-
-            # Save the output PDF to the output folder
-            output_path = os.path.join(output_path, file)
-            # ...
-
-
-def create_data_structure(pdf_data):
-    """    # Create a pandas DataFrame with the data from the PDF
-    data = {
-        'column1': [value1, value2, ...],
-        'column2': [value1, value2, ...],
-        ...
-    }
-    df = pd.DataFrame(data)
-
-    # Perform any necessary data processing, calculations, and transformations using pandas functions
-    pass"""
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from Processing_Functions import (  # import the functions from the new file
+    extract_and_process_pdf,
+    create_output_pdf,
+)
 
 
 def process_all_pdfs(in_folder: str, out_folder: str):
@@ -67,23 +20,25 @@ def process_all_pdfs(in_folder: str, out_folder: str):
         print("Files in folder:", files_in_folder)
         counter = 0
 
-        for file in files_in_folder:
-            if file.endswith(".pdf"):
+        for pdf_file in files_in_folder:  # Change the variable name here
+            if pdf_file.endswith(".pdf"):
                 counter += 1
-                print(f"Processing file {counter}: {file}")
-                input_path = Path(in_folder) / file
-                output_path = Path(out_folder) / file
-                print(input_path)
-                print(output_path)
+                print(f"Processing file {counter}: {pdf_file}")
+                input_path = Path(in_folder) / pdf_file
+                output_path = Path(out_folder) / pdf_file
                 # Submit the task to extract and process the PDF into a DataFrame
                 future = executor.submit(extract_and_process_pdf, str(input_path))
                 future.output_path = str(output_path)
                 futures.append(future)
 
-        for future in futures.as_completed(futures):
+        for future in as_completed(futures):
             try:
-                pdf_data = future.result()
-                print(pdf_data, future.output_path)
+                rfc, pdf_data = future.result()
+                output_path = Path(out_folder) / f"{rfc}.pdf"
+                print(pdf_data, '\n', output_path)
+
+                # Call the create_output_pdf() function with the extracted data and output path
+                create_output_pdf(pdf_data, str(output_path))
+
             except Exception as e:
                 print(f"An error occurred: {e}")
-
